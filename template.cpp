3254479a9833adbb0ed0ee7bb83478b8b8586443
Muhammad Indra Ramadhan <mindrar96@gmail.com>
2020-11-28 17:19:07 +0700

    Add gitignore and c++ template

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+
+#define ll long long
+
+#define MAXN 100005
+#define MOD 1000000007
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+
+  return 0;
+}
